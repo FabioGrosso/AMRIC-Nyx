8b43af7914627f8237622121452c04c47c7b9442
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2023-06-21 21:30:42 -0700

    Update sz2.sh

--- Exec/AMR-density/sz2.sh
+++ Exec/AMR-density/sz2.sh
@@ -1,5 +1,4 @@
-export HDF5_PLUGIN_PATH=/opt/SZ_SLE/install/lib
-export LD_LIBRARY_PATH=/.singularity.d/libs
-export LD_LIBRARY_PATH=/opt/SZ_SLE/install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/hdf5-1.12.2/install/lib:$LD_LIBRARY_PATH
-export PATH=$PATH:/opt/hdf5-1.12.2/install/bin
+export HDF5_PLUGIN_PATH=$SZ_SLE_HOME/lib
+export LD_LIBRARY_PATH=$SZ_SLE_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$AMRIC_H5_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:$AMRIC_H5_HOME/bin
