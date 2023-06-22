f5e8c6d1c69b953c9ab9e210e87f6c6b98320030
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2023-06-21 21:33:21 -0700

    Update bash.sh

--- Exec/AMR-density/bash.sh
+++ Exec/AMR-density/bash.sh
@@ -5,23 +5,23 @@ do
     echo "0.001" > eb.txt
     echo "sz2"
     source ./sz2.sh
-    mpirun -np 8 ./sz2.ex inputs > otfile/sz2-$i.txt
+    mpirun -np 8 ./sz2 inputs > otfile/sz2-$i.txt
     mv run/plt00211_multi.h5 run/sz2-001.h5
 
     echo "0.001" > eb.txt
     echo "sz3"
     source ./sz3.sh
-    mpirun -np 8 ./sz3.ex inputs > otfile/sz3-$i.txt
+    mpirun -np 8 ./sz3 inputs > otfile/sz3-$i.txt
     mv run/plt00211_multi.h5  run/sz3-001.h5
 
     echo "0.005" > eb.txt
     source ./base.sh
     echo "nocomp"
-    mpirun -np 8 ./nocomp.ex inputs > otfile/nocomp-$i.txt
+    mpirun -np 8 ./nocomp inputs > otfile/nocomp-$i.txt
     mv run/plt00211_multi.h5  run/nocomp.h5
 
     echo "comp"
-    mpirun -np 8 ./comp.ex inputs > otfile/comp-$i.txt
+    mpirun -np 8 ./comp inputs > otfile/comp-$i.txt
     mv run/plt00211_multi.h5  run/comp-005.h5
 done
 rm -rf run/*old*
