0e36a5cd1c01f286873cdda4ade80a6853580156
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2023-06-21 21:32:46 -0700

    Update base.sh

--- Exec/AMR-density/base.sh
+++ Exec/AMR-density/base.sh
@@ -1,5 +1,6 @@
-export HDF5_PLUGIN_PATH=/opt/orisz3/install/lib
-export LD_LIBRARY_PATH=/.singularity.d/libs
-export LD_LIBRARY_PATH=/opt/orisz3/install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/hdf5-1.12.2/install/lib:$LD_LIBRARY_PATH
-export PATH=$PATH:/opt/hdf5-1.12.2/install/bin
+export HDF5_PLUGIN_PATH=$ORI_SZ3_HOME/lib
+export LD_LIBRARY_PATH=$ORI_SZ3_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$AMRIC_H5_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:$AMRIC_H5_HOME/bin
+
+
